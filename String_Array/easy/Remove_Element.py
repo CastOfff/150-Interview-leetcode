0a895ff1e0e682a8729932978bb6ea0e7999ce75
CastCast <lamanhcuong.hus@gmail.com>
2024-06-18 11:12:16 +0700

    change RE problem

--- String_Array/easy/Remove_Element.py
+++ String_Array/easy/Remove_Element.py
@@ -1,12 +1,24 @@
 class Solution(object):
     def removeElement(self, nums, val):
-        new_nums = []
-        for i in range(len(nums)):
-            if nums[i] != val:
-                new_nums.append(nums[i])
-        return len(new_nums)
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
+        n = len(nums)
+        count = 0
+        while count < n:
+            if nums[count] == val:
+                for i in range(count, n-1):
+                    nums[i] = nums[i+1]
+                n -= 1
+            else:
+                count += 1
+        return n
+    
+test_1 = Solution()
+nums = [3,2,2,3]
+val = 3
+print(test_1.removeElement(nums, val))
+
+
+"""
+:type nums: List[int]
+:type val: int
+:rtype: int
+"""
